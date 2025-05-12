ca46e110743e11b61d66697e56cd122e45ed14a6
AdrianS2002 <aschiop2002@yahoo.com>
2025-05-12 19:08:43 +0300

    added vercel json

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,8 +1,9 @@
+// src/app/services/github.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, forkJoin } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
-import { environment } from '../environments/environment.js';
+import { environment } from '../environments/environment';
 
 export interface Repo {
   id: number;
@@ -15,18 +16,22 @@ export interface Repo {
   languages: string[];
 }
 
-
 @Injectable({ providedIn: 'root' })
 export class GithubService {
   private apiUrl = 'https://api.github.com/users';
   private username = 'AdrianS2002';
-  private authHeaders = new HttpHeaders({
-    Authorization: `token ${environment.githubToken}`, 
-    Accept: 'application/vnd.github.mercy-preview+json'
-  });
 
   constructor(private http: HttpClient) {}
 
+  private getAuthHeaders(): HttpHeaders | undefined {
+    const token = environment.githubToken;
+    return token
+      ? new HttpHeaders({
+          Authorization: `token ${token}`,
+          Accept: 'application/vnd.github.mercy-preview+json'
+        })
+      : undefined;
+  }
 
   getReposWithLanguages(): Observable<Repo[]> {
     const params = new HttpParams()
@@ -34,39 +39,41 @@ export class GithubService {
       .set('direction', 'desc')
       .set('per_page', '100');
 
+    const headers = this.getAuthHeaders();
+
+ 
     return this.http
-      .get<Repo[]>(`${this.apiUrl}/${this.username}/repos`, {
-        params,
-        headers: this.authHeaders
-      })
+      .get<Repo[]>(`${this.apiUrl}/${this.username}/repos`, { params, headers })
       .pipe(
+       
         mergeMap(repos =>
           forkJoin(
-            repos.map(repo =>
-              this.http
+            repos.map(repo => {
+              const langHeaders = this.getAuthHeaders();
+              return this.http
                 .get<Record<string, number>>(
                   `https://api.github.com/repos/${this.username}/${repo.name}/languages`,
-                  { headers: this.authHeaders }
+                  { headers: langHeaders }
                 )
                 .pipe(
                   map(langObj => ({
                     ...repo,
                     languages: Object.keys(langObj)
                   }))
-                )
-            )
+                );
+            })
           )
         )
       );
   }
 
-   getReadmeRawUrl(repoName: string): string {
+  getReadmeRawUrl(repoName: string): string {
     return `https://raw.githubusercontent.com/${this.username}/${repoName}/main/README.md`;
   }
 
   getReadmeMarkdown(repoName: string): Observable<string> {
     const url = this.getReadmeRawUrl(repoName);
+    // pentru raw markdown nu avem nevoie de autentificare
     return this.http.get(url, { responseType: 'text' });
   }
-
 }
