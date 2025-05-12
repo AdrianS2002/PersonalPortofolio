2aa9565f9b81b4cbddbd9451806e967fbfa1d512
AdrianS2002 <aschiop2002@yahoo.com>
2025-05-12 18:24:48 +0300

    Add environment.prod.ts for production builds

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, forkJoin } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 export interface Repo {
   id: number;
